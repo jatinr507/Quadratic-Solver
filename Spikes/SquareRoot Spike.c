b1f133c1ffc32bf852062269807c1e9a2c05952b
Jay Sharma <jatinr507@gmail.com>
2016-11-14 17:54:14 -0500

    Spike for Square root for doubles

---
+++ Spikes/SquareRoot Spike.c
@@ -0,0 +1,27 @@
+// Spike for Square root for doubles.
+#include <math.h>
+
+int main() {
+
+double a,b,c,d,e,f,g,h,i,j,k;
+
+a=sqrt(0);
+b=sqrt(1);
+c=sqrt(2);
+d=sqrt(.2);
+e=sqrt(1/3);
+f=sqrt(.3333);
+g=sqrt(456789123);
+
+
+printf("sqrt(0): %f\n",a);
+printf("sqrt(1): %f\n",b);
+printf("sqrt(2): %f\n",c);
+printf("sqrt(.2): %f\n",d);
+printf("sqrt(1/3): %f\n",e);
+printf("sqrt(.3333): %f\n",f);
+printf("sqrt(456789123): %f\n",g);
+
+
+return 0;
+}
