32c0a336739d317b1d90b589105b3e9ca2f8ac44
Jatin Sharma <jatinr507@wireless147-077.wireless.wmich.edu>
2016-11-15 11:06:12 -0500

    src files

---
+++ Source/HFiles/structDec.h
@@ -0,0 +1,13 @@
+/*
+	Holds definitions for the structs used for all of the modules. 
+*/
+typedef struct{
+	double x1;
+	double x2;
+	int valid;
+}Roots;
+typedef struct{
+	double a;
+	double b;
+	double c;
+}Inputs;
